8107d3fed5ac09f6923fb97c44235d1edced6ef3
AgentReckless <bogdan.popovic0@hotmail.rs>
2026-06-19 20:06:54 +0200

    foundation added

---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class Constants {
+    public static final String GOOGLE_URL = "https://www.google.com";
+    public static final String PA_URL = "https://www.polovniautomobili.com";
+}
